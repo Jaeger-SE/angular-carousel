ebf926101b350b7b2fdb54e5b6c6db126cb68916
Clément Rigo <rigoclement@mydnic.be>
2015-12-09 14:41:19 +0100

    Proper use of compile functions

--- carousel.js
+++ carousel.js
@@ -51,7 +51,7 @@
                 }
             }
             return {
-                'background-image': "url(" + item.image_path + ")",
+                'background-image': "url(" + item.src + ")",
                 'transform': "translateX(" + percentage + "%)",
                 'z-index': getZIndex(index)
             };
@@ -79,7 +79,7 @@
 
         //////////////////////////////////
 
-        vm.restart();
+        // vm.restart();
     }
 
     carouselController.$inject = ["$interval"];
@@ -106,53 +106,60 @@
                 if (!attr.carouselDuration) {
                     attr.$set("carouselDuration", "10");
                 }
-                return function($scope, $element, $attr) {
-                    var myLoop = $attr.carousel,
-                        match = myLoop.match(/^\s*(.+)\s+in\s+(.*?)\s*(\s+track\s+by\s+(.+)\s*)?$/),
-                        indexString = match[1],
-                        collectionString = match[2];
+                return{
+                    pre: function preLink($scope, $element, $attr, $controller) {
 
-                    var container = "<div style=\"overflow:hidden;position:relative;height:100%;overflow:hidden;position:relative;height:100%;\">" +
-                        "<ul style=\"height: 100%;list-style: none;margin: 0;padding: 0;\">" +
-                        "</ul>" +
-                        "</div>";
-                    $element.append(container);
+                    },
+                    post: function postLink($scope, $element, $attr, $controller) {
+                        var myLoop = $attr.carousel,
+                            match = myLoop.match(/^\s*(.+)\s+in\s+(.*?)\s*(\s+track\s+by\s+(.+)\s*)?$/),
+                            indexString = match[1],
+                            collectionString = match[2];
 
-                    var parent = $element.find('ul');
+                        var container = "<div style=\"overflow:hidden;position:relative;height:100%;\">" +
+                            "<ul style=\"height: 100%;list-style: none;margin: 0;padding: 0;\">" +
+                            "</ul>" +
+                            "</div>";
+                        $element.append(container);
 
-                    $scope.$parent.$watchCollection(collectionString, function (collection) {
+                        var parent = $element.find('ul');
 
-                        for (var i = 0; i < collection.length; i++) {
-                            var childScope = $scope.$new();
+                        $scope.$parent.$watchCollection(collectionString, function (collection) {
 
-                            childScope[indexString] = collection[i];
+                            for (var i = 0; i < collection.length; i++) {
+                                var childScope = $scope.$new();
 
-                            linker(childScope, function (clone) {
+                                childScope[indexString] = collection[i];
 
-                                var listItem = "<li " +
-                                               "style=\"position: absolute; width: 100%; height: 100%; display: block; transition: opacity 0.7s ease-in, transform 1s linear; background-size: cover; background-position: 50% 0%;\" " +
-                                               "data-ng-mouseenter=\"carousel.pause()\" " +
-                                               "data-ng-mouseleave=\"carousel.restart()\" " +
-                                               "data-ng-style=\"caroussel.getStyle(childScope[indexString], $index)\" " +
-                                               "data-ng-class=\"{'active': $index==caroussel.activeIndex}\">" +
-                                               "</li>";
-                                
-                                // clone the transcluded element, passing in the new scope.
-                                parent.append(listItem); // add to DOM
-                                
-                                parent.find('li:last-child').append(clone);
+                                linker(childScope, function (clone) {
 
-                                var block = {};
-                                block.el = listItem;
-                                block.scope = childScope;
-                                $scope.carousel.carouselItems.push(block);
-                            });
-                        }
-                    });
+                                    var listItem = "<li " +
+                                                   "style=\"position: absolute; width: 100%; height: 100%; display: block; transition: opacity 0.7s ease-in, transform 1s linear; background-size: cover; background-position: 50% 0%;\" " +
+                                                   "data-ng-mouseenter=\"carousel.pause()\" " +
+                                                   "data-ng-mouseleave=\"carousel.restart()\" " +
+                                                   "data-ng-click=\"alert('coucou')\" " +
+                                                   "data-ng-style=\"carousel.getStyle(childScope[indexString], $index)\" " +
+                                                   "data-ng-class=\"{'active': $index==carousel.activeIndex}\">" +
+                                                   "</li>";
+
+                                    // clone the transcluded element, passing in the new scope.
+                                    parent.append(listItem); // add to DOM
+                                    
+                                    parent.find('li:last-child').append(clone);
+
+                                    var block = {};
+                                    block.el = listItem;
+                                    block.scope = childScope;
+                                    $controller.carouselItems.push(block);
+                                });
+                            }
+                            $controller.restart();
+                        });
+                    }
                 };
             }
         };
-    };
+    }
 
     carouselDirective.$inject = [];
 
