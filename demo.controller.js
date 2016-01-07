e83102df57a04da2a65595b193e19415c473be55
Clément Rigo <rigoclement@mydnic.be>
2016-01-07 16:33:52 +0100

    Work on multiple items

--- demo.controller.js
+++ demo.controller.js
@@ -11,27 +11,27 @@
         vm.slides = [
             {
                 src: 'http://placehold.it/1140x250?text=May',
-                title: 'Slide 1'
+                title: 'Slide 1 (0)'
             },
             {
                 src: 'http://placehold.it/1140x250?text=The',
-                title: 'Slide 2'
+                title: 'Slide 2 (1)'
             },
             {
                 src: 'http://placehold.it/1140x250?text=Force',
-                title: 'Slide 3'
+                title: 'Slide 3 (2)'
             },
             {
                 src: 'http://placehold.it/1140x250?text=Be',
-                title: 'Slide 4'
+                title: 'Slide 4 (3)'
             },
             {
                 src: 'http://placehold.it/1140x250?text=With',
-                title: 'Slide 5'
+                title: 'Slide 5 (4)'
             },
             {
                 src: 'http://placehold.it/1140x250?text=You',
-                title: 'Slide 6'
+                title: 'Slide 6 (5)'
             },
         ];
     }
